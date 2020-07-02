cd792b923ee26ff4416a2fa080108d4e06131912
Max Huang <letmaxknow@gmail.com>
2020-07-02 13:08:31 -0400

    Added search and pagination layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { Image, Header, Modal, Icon, Button, Table, Label } from 'semantic-ui-react';
+import { Image, Header, Modal, Icon, Button, Table, Label, Pagination, Input } from 'semantic-ui-react';
 
 import stylesheet from './css/App.module.css';
 
-function App() {
-  return (
-    <div className={stylesheet.wrapper}>
-      <Item title='Maze 1' />
-      <Item title='Maze 2' />
-      <Item title='Maze 3' />
-      <Item title='Maze 4' />
-      <Item title='Maze 5' />
-      <Item title='Maze 6' />
-      <Item title='Maze 7' />
-      <Item title='Maze 8' />
-    </div>
-  );
+class App extends React.Component {
+  
+  input = '';
+
+  inputFunc = (e, { name, value }) => {
+    this.input = value;
+  }
+
+  clickFunc = () => {
+    alert(JSON.stringify(this.input.split(' ').filter(val => val !== '')));
+  }
+
+  render() {
+    return (
+      <div className={stylesheet.wrapper}>
+        <Header>Search</Header>
+        <div className={stylesheet.wrapper__search}>
+          <Input onChange={this.inputFunc} action={{ icon: 'search', primary: true, onClick: this.clickFunc }} placeholder='Search for tags and creators' fluid />
+        </div>
+        <Header>Results</Header>
+        <div className={stylesheet.wrapper__results}>
+          <Item title='Maze 1' />
+          <Item title='Maze 2' />
+          <Item title='Maze 3' />
+          <Item title='Maze 4' />
+          <Item title='Maze 5' />
+          <Item title='Maze 6' />
+          <Item title='Maze 7' />
+          <Item title='Maze 8' />
+          <Item title='Maze 9' />
+          <Item title='Maze 10' />
+          <Item title='Maze 11' />
+          <Item title='Maze 12' />
+        </div>
+        <div className={stylesheet.wrapper__pagination}>
+          <Pagination defaultActivePage={1} totalPages={10} />
+        </div>
+      </div>
+    );
+  }
 }
 
 class Item extends React.Component {
@@ -32,7 +59,7 @@ class Item extends React.Component {
     return (
       <>
         <div className={stylesheet.itemCard} onClick={() => this.openModal()}>
-          <div className={stylesheet.itemCard__image__wrapper}>
+          <div className={stylesheet.itemCard__image}>
             <div>
               <img src='https://picsum.photos/600' alt='Thumbnail' />
             </div>
@@ -50,16 +77,14 @@ class Item extends React.Component {
                 <Table.Row>
                   <Table.Cell>Creator</Table.Cell>
                   <Table.Cell>
-                    <a href='#'>max-y-huang</a>
+                    <a href='/max-y-huang'>max-y-huang</a>
                   </Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>Tag(s)</Table.Cell>
                   <Table.Cell>
-                    <Label.Group>
-                      <Label as='a' href='#' content='Tag_1' />
-                      <Label as='a' href='#' content='Tag_2' />
-                    </Label.Group>
+                    <Label as='a' href='#' content='Tag_1' basic />
+                    <Label as='a' href='#' content='Tag_2' basic />
                   </Table.Cell>
                 </Table.Row>
               </Table.Body>
