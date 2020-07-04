fede932eb823a3ef4cee2018ffdfdf43ba859725
Max Huang <letmaxknow@gmail.com>
2020-07-04 15:38:47 -0400

    Added api calls

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React from 'react';
 import { Image, Header, Modal, Icon, Button, Table, Label, Pagination, Input } from 'semantic-ui-react';
+import axios from 'axios';
+import queryString from 'query-string';
 
 import stylesheet from './css/App.module.css';
 
-class App extends React.Component {
-  
-  input = '';
+import urls from './utils/urls';
 
-  inputFunc = (e, { name, value }) => {
-    this.input = value;
+class App extends React.Component {
+
+  searchBarValue = '';
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemData: null
+    };
   }
 
-  clickFunc = () => {
-    alert(JSON.stringify(this.input.split(' ').filter(val => val !== '')));
+  searchBarOnChange = (e) => {
+    this.searchBarValue = e.target.value;
+  }
+  searchBarOnKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      this.searchTags();
+    }
+  }
+
+  searchTags = () => {
+    window.open(`?tags=${this.searchBarValue}`, '_self');
+  }
+
+  getData = () => {
+    axios.get(`${urls.mazeNotepadApi}/maze/get${window.location.search}`).then(res => {
+      this.setState({ itemData: res.data.result });
+    });
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  renderItems = () => {
+    if (!this.state.itemData || !this.state.itemData[0]) {
+      return (
+        <p>No mazes found.</p>
+      );
+    }
+
+    return this.state.itemData.map((item, i) => {
+      let mazeNotepadUrl = `${urls.mazeNotepadJs}?maze=${item['maze-file-name']}`;
+      let imageUrl       = `${urls.mazeNotepadApi}/uploads/${item['image-file-name']}`;
+      return (
+        <Item
+          key={i}
+          title={item['name']}
+          mazeNotepadUrl={mazeNotepadUrl}
+          imageUrl={imageUrl}
+          tags={item['tags']} />
+      );
+    });
   }
 
   render() {
@@ -20,22 +68,18 @@ class App extends React.Component {
       <div className={stylesheet.wrapper}>
         <Header>Search</Header>
         <div className={stylesheet.wrapper__search}>
-          <Input onChange={this.inputFunc} action={{ icon: 'search', primary: true, onClick: this.clickFunc }} placeholder='Search for tags and creators' fluid />
+          <Input
+            onChange={this.searchBarOnChange}
+            onKeyUp={this.searchBarOnKeyUp}
+            action={{ icon: 'search', primary: true, onClick: this.clickFunc }}
+            defaultValue={queryString.parse(window.location.search)['tags']}
+            placeholder='Search for tags and creators'
+            fluid
+          />
         </div>
         <Header>Results</Header>
         <div className={stylesheet.wrapper__results}>
-          <Item title='Maze 1' />
-          <Item title='Maze 2' />
-          <Item title='Maze 3' />
-          <Item title='Maze 4' />
-          <Item title='Maze 5' />
-          <Item title='Maze 6' />
-          <Item title='Maze 7' />
-          <Item title='Maze 8' />
-          <Item title='Maze 9' />
-          <Item title='Maze 10' />
-          <Item title='Maze 11' />
-          <Item title='Maze 12' />
+          {this.renderItems()}
         </div>
         <div className={stylesheet.wrapper__pagination}>
           <Pagination defaultActivePage={1} totalPages={10} />
@@ -54,42 +98,41 @@ class Item extends React.Component {
   openModal = () => this.setState({ modalOpen: true });
   closeModal = () => this.setState({ modalOpen: false });
 
+  renderTags = () => {
+    return this.props.tags.map((tag, i) => {
+      return <Label key={i} as='a' href={`?tags=${tag}`} content={tag} basic />
+    });
+  }
+
   render() {
 
     return (
       <>
-        <div className={stylesheet.itemCard} onClick={() => this.openModal()}>
-          <div className={stylesheet.itemCard__image}>
+        <div className={stylesheet.itemCard}>
+          <div className={stylesheet.itemCard__image} onClick={() => this.openModal()}>
             <div>
-              <img src='https://picsum.photos/600' alt='Thumbnail' />
+              <img src={this.props.imageUrl} alt='Thumbnail' />
             </div>
           </div>
-          <Header className={stylesheet.itemCard__header}>
+          <Header className={stylesheet.itemCard__header} onClick={() => this.openModal()}>
             {this.props.title}
           </Header>
         </div>
         <Modal open={this.state.modalOpen}>
           <Modal.Header>{this.props.title}</Modal.Header>
           <Modal.Content scrolling>
-            <Image as ='a' href='https://picsum.photos/600' target='_blank' src='https://picsum.photos/600' fluid />
+            <Image target='_blank' src={this.props.imageUrl} fluid />
             <Table celled definition>
               <Table.Body>
                 <Table.Row>
-                  <Table.Cell>Creator</Table.Cell>
-                  <Table.Cell>
-                    <a href='/max-y-huang'>max-y-huang</a>
-                  </Table.Cell>
-                </Table.Row>
-                <Table.Row>
                   <Table.Cell>Tag(s)</Table.Cell>
                   <Table.Cell>
-                    <Label as='a' href='#' content='Tag_1' basic />
-                    <Label as='a' href='#' content='Tag_2' basic />
+                    {this.renderTags()}
                   </Table.Cell>
                 </Table.Row>
               </Table.Body>
             </Table>
-            <Button as='a' href='https://max-y-huang.github.io/maze-notepad-js' target='_blank' primary fluid>Open in Maze Notepad</Button>
+            <Button as='a' href={this.props.mazeNotepadUrl} target='_blank' primary fluid>Open in Maze Notepad</Button>
           </Modal.Content>
           <Modal.Actions>
             <Button primary onClick={() => this.closeModal()}>
---
+++ src/utils/urls.js
@@ -0,0 +1,4 @@
+const mazeNotepadJs  = 'https://max-y-huang.github.io/maze-notepad-js';
+const mazeNotepadApi = 'https://maze-notepad-api.herokuapp.com';
+
+module.exports = { mazeNotepadJs, mazeNotepadApi };
