2b32b5e7bc38c52081bde48760cbb663814aaa96
max-y-huang <letmaxknow@gmail.com>
2022-05-20 03:10:41 -0400

    updated package.json

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,7 +1,5 @@
-const mazeNotepadJs  = 'https://max-y-huang.github.io/maze-notepad-js';
-//const mazeNotepadJs  = 'http://localhost:3000/maze-notepad-js';
-const mazeNotepadApi = 'https://maze-notepad-api.herokuapp.com';
-//const mazeNotepadApi = 'http://localhost:8000';
-const s3Bucket = 'https://maze-notepad.s3.us-east-2.amazonaws.com';
+const mazeNotepadJs = "https://maze-notepad.netlify.app";
+const mazeNotepadApi = "https://maze-notepad-api.herokuapp.com";
+const s3Bucket = "https://maze-notepad.s3.us-east-2.amazonaws.com";
 
 module.exports = { mazeNotepadJs, mazeNotepadApi, s3Bucket };
